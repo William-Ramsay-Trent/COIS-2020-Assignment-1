d385feda9ab2e806a037c153b4b33ab8e01140c4
William Ramsay <williamramsay@trentu.ca>
2023-09-27 17:51:00 -0400

    Reformatting

--- Polynomials.cs
+++ Polynomials.cs
@@ -5,24 +5,37 @@ class Polynomials
     // A collection of polynomials
     private List<Polynomial> L;
     // Creates an empty list L of polynomials
-    public Polynomials()
-    {}
+    
+public Polynomials()
+{
+    
+}
     
 // Retrieves the polynomial stored at position i in L
 public Polynomial Retrieve(int i) 
-{}
+{
+
+}
 // Inserts polynomial p into L
 public void Insert(Polynomial p) 
-{}
+{
+    
+}
 // Deletes the polynomial at index i
 public void Delete(int i) 
-{}
+{
+    
+}
 // Returns the number of polynomials in L
 public int Size()
-    {}
+{
+    
+}
     
 // Prints out the list of polynomials
 public void Print()
-    {}
-  
+{
+    
 }
+  
+}//EOC
